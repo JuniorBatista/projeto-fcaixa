e4fd6056599ab13a79739851d73b479a0c30a89c
Juvenal Marcos Batista (Junior) <juvenalmarcos@gmail.com>
2020-08-08 21:54:40 -0300

    Arquivos iniciais do Projeto

---
+++ src/main/java/com/slaker/fcaixa/FcaixaApplication.java
@@ -0,0 +1,13 @@
+package com.slaker.fcaixa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FcaixaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FcaixaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/slaker/fcaixa/FcaixaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slaker.fcaixa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FcaixaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
